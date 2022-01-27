916dbb6af13e669a867ec6083829b92dbfcd2e46
Sideways.Reptile <impressive-tie@pm.me>
2022-01-27 14:39:14 -0800

    Pushing Day 16 code to Repo.
    Used mv, cp, rm to make a version more in line with new conventions.

---
+++ Day16-CoffeeMachine/coffee_maker.py
@@ -0,0 +1,39 @@
+from prettytable import PrettyTable
+table = PrettyTable()
+
+
+class CoffeeMaker:
+    """Models the machine that makes the coffee"""
+    def __init__(self):
+        self.resources = {
+            "water": 300,
+            "milk": 200,
+            "coffee": 100,
+        }
+
+    def report(self):
+        """Prints a report of all resources."""
+        """Updated with PrettyTables formatting."""
+        table.clear()
+        table.add_column("Ingredients", ["water", "milk", "coffee"])
+        table.add_column("Amount", [f"{self.resources['water']}ml", f"{self.resources['milk']}ml",
+                                    f"{self.resources['coffee']}ml"])
+        print(table)
+        #print(f"Water: {self.resources['water']}ml")
+        #print(f"Milk: {self.resources['milk']}ml")
+        #print(f"Coffee: {self.resources['coffee']}g")
+
+    def is_resource_sufficient(self, drink):
+        """Returns True when order can be made, False if ingredients are insufficient."""
+        can_make = True
+        for item in drink.ingredients:
+            if drink.ingredients[item] > self.resources[item]:
+                print(f"Sorry there is not enough {item}.")
+                can_make = False
+        return can_make
+
+    def make_coffee(self, order):
+        """Deducts the required ingredients from the resources."""
+        for item in order.ingredients:
+            self.resources[item] -= order.ingredients[item]
+        print(f"Here is your {order.name} ☕️. Enjoy!")
