6d5d42193cb3211948b13455051af6bae74252b0
Sideways.Reptile <impressive-tie@pm.me>
2022-02-26 23:57:17 -0800

    Renamed
    also pushing Day 57 - Jinja

---
+++ Day 54-IntroToFlaskDev/README.md
@@ -0,0 +1,9 @@
+# Notes
+
+Decorators are functions that allow use to make better use
+of other functions in our code. This way, if we want to make
+some changes to how a function is called and therefore runs,
+a decorator can be used. Essentially, we can pass other functions
+in as aspects of the decorator. In this code, 
+the fast and slow functions are being timed ia code in their 
+decorator. 
---
+++ Day 54-IntroToFlaskDev/main.py
@@ -0,0 +1,42 @@
+from flask import Flask
+import time
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return 'Hello, World!'
+
+
+if __name__ == "__main__":
+    app.run()
+
+############# CODE CHALLENGE ##########
+# PYTHON DECORATORS
+
+
+
+current_time = time.time()
+print(current_time)
+
+
+def speed_calc_decorator(function):
+    def wrapper_function():
+        start_time = time.time()
+        function()
+        end_time = time.time()
+        print(f"{function.__name__} run speed: {end_time - start_time}s")
+
+    return wrapper_function()
+
+
+@speed_calc_decorator
+def fast_function():
+    for i in range(10000000):
+        i * i
+
+
+@speed_calc_decorator
+def slow_function():
+    for i in range(100000000):
+        i * i
